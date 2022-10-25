5d60565adacf2b6197edcca8fdf1788a2d8476c2
Tim Anderson <tim.anderson@groundtruth.com>
2022-10-25 16:16:34 -0700

    remove the slack bot

--- release.config.js
+++ release.config.js
@@ -15,26 +15,5 @@ module.exports = {
         { message: "chore(release): ${nextRelease.version}" },
       ],
       "@semantic-release/github",
-      [
-        "semantic-release-slack-bot",
-        {
-          "notifyOnSuccess": false,
-          "onSuccessTemplate": {
-            "text": "A new version of $package_name with version $npm_package_version has been released at $repo_url!"
-          },
-          "notifyOnFail": false,
-          "slackWebhook": "https://hooks.slack.com/services/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",
-          "branchesConfig": [
-            {
-              "pattern": "main",
-              "notifyOnSuccess": true,
-              "notifyOnFail": true,
-              "onSuccessTemplate": {
-                "text": "A new version of $package_name with version $npm_package_version has been released at $repo_url!"
-              }
-            }
-          ]
-        }
-      ],
     ],
   };
