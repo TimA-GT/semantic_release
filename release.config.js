901c07ca94d35413b5cbe9cc340080a14283595b
Tim Anderson <tim.anderson@groundtruth.com>
2022-10-25 16:11:41 -0700

    fix: test slack web hook deactivates

--- release.config.js
+++ release.config.js
@@ -23,7 +23,7 @@ module.exports = {
             "text": "A new version of $package_name with version $npm_package_version has been released at $repo_url!"
           },
           "notifyOnFail": false,
-          "slackWebhook": "https://hooks.slack.com/services/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",
+          "slackWebhook": "https://hooks.slack.com/services/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",
           "branchesConfig": [
             {
               "pattern": "main",
