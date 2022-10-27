d8a0ada0055f88804a18b43e5ee70601fd7d161f
Tim Anderson <tim.anderson@groundtruth.com>
2022-10-27 13:55:30 -0700

    breaking: added workflows for POC and updated regex for PR title checker

--- release.config.js
+++ release.config.js
@@ -1,7 +1,13 @@
 module.exports = {
     branches: ["main"],
     plugins: [
-      "@semantic-release/commit-analyzer",
+      ["@semantic-release/commit-analyzer", {
+        "preset": "angular",
+        "releaseRules": [
+            {"type": "breaking", "release": "major"},
+            {"scope": "no-release", "release": false}
+        ]
+      }],
       "@semantic-release/release-notes-generator",
       "@semantic-release/changelog",
       [
